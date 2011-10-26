a7d3e4c50bb83d0679e68ebb1f89e3291dfe6332
Andrew Thompson <andrew@hijacked.us>
2011-10-26 16:16:56 -0400

    Commit project skeleton

---
+++ src/eradiusd_app.erl
@@ -0,0 +1,16 @@
+-module(eradiusd_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    eradiusd_sup:start_link().
+
+stop(_State) ->
+    ok.

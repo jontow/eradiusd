fbcf3f2df0fd649bbb755ab92ab965019e138d36
Jonathan Towne <jontow@zenbsd.net>
2011-10-26 21:39:37 -0400

    remove duplicate start of eradiusd

--- src/eradiusd_sup.erl
+++ src/eradiusd_sup.erl
@@ -26,7 +26,6 @@ start_link() ->
 init([]) ->
 	{ok, {{one_for_one, 5, 10}, [
 		{eradius_server, {eradius_server, start_link, []}, permanent, 10000, worker, [eradius_server]},
-		{eradius_dict, {eradius_dict, start_link, []}, permanent, 10000, worker, [erdius_dict]},
-		{eradiusd, {eradiusd, start_link, []}, permanent, 10000, worker, [eradiusd]}
+		{eradius_dict, {eradius_dict, start_link, []}, permanent, 10000, worker, [erdius_dict]}
 	] }}.
 

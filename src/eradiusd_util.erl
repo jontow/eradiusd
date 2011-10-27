c0c01425c22a5e1dab9d48436fcee56db87d0491
Jonathan Towne <jontow@zenbsd.net>
2011-10-26 22:05:29 -0400

    push up some more changes

--- src/eradiusd_util.erl
+++ src/eradiusd_util.erl
@@ -2,6 +2,10 @@
 
 -export([init/0]).
 
+-include_lib("eradius/include/eradius_lib.hrl").
+-include_lib("eradius/include/eradius_dict.hrl").
+-include_lib("eradius/include/dictionary.hrl").
+
 init() ->
 	eradius_server:create_tables([node()]),
 	load_config(),
@@ -10,4 +14,9 @@ init() ->
 load_config() ->
 	% this should be from a config file..
 	io:format("Loading config~n"),
-	eradius_server:define_ras({127,0,0,1}, 1812, "secret", {server, test}).
+	eradius_server:define_ras({127,0,0,1}, 1812, "secret", {eradiusd_util, test}).
+
+% Minimal (!) example of Access Request handler
+test(#rad_pdu{}, #nas_prop{}) ->
+    io:format("Test spawned~n"),
+    #rad_accept{}.
